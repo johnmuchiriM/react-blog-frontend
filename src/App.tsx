f9a4b9198a52e17b68cb589b86fcfef9316e8cf3
johnmuchiriM <jeanfrancaiseharicot@gmail.com>
2022-08-26 08:13:33 -0700

    update:update on getting data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,7 @@ import './App.css';
 import { PostAPI } from './api/post.api';
 import { CreatePostDto } from './api/dto/post.dto';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 
 function App() {
@@ -31,110 +20,27 @@ function App() {
       console.log("The error is :"+error.message)
   });
   },[])
-  const theme = createTheme();
 
  return (
   
  
-    // <div className="App">
-    //    <ul>
-    //   {
-    //      posts.map(post => {
-    //       return <li key={post.id}>{post.title},{post.id}</li>
+    <div className="App">
+       <ul>
+      {
+         posts.map(post => {
+          return <li key={post.id}>{post.title},{post.id}</li>
 
         
 
           
-    //     })
-    //   }
-    //   <Button variant="contained">Create</Button>
-    //    </ul>
+        })
+      }
+      <Button variant="contained">Create Post</Button>
+       </ul>
 
-    // </div>
+    </div>
 
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            {/* <LockOutlinedIcon /> */}
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Create a Post
-          </Typography>
-          <Box component="form" noValidate  sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Create Post
-            </Button>
-            <Grid container justifyContent="flex-end">
-             
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+   
   );
 
 
